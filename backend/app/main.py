96f7615ec74264db8f49833245287bd663877a7f
narutouzumak1 <gtambale005@gmail.com>
2026-09-23 16:15:59 +0530

    add cors in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://miraculous-truth-production-8590.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
